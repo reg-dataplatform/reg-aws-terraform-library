5be6f339fbcdb762ded271a41625f443cc749655
Christian-Gudbrandsen <christian.gudbrandsen@ren.oslo.kommune.no>
2020-06-07 22:51:59 +0200

    first commit

---
+++ lambda/main.tf
@@ -0,0 +1,20 @@
+data "archive_file" "init" {
+  type        = "zip"
+  source_dir  = var.lambda_script_source_dir
+  output_path = join("", [var.lambda_script_output_path, var.child_module, ".zip"])
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  filename         = join("", [var.lambda_script_output_path, var.child_module, ".zip"])
+  function_name    = join("", [basename(var.parent_module_path), "-lambda-", var.child_module])
+  role             = var.iam_role_arn
+  handler          = var.lambda_handler
+  source_code_hash = filebase64sha256(join("", [var.lambda_script_output_path, var.child_module, ".zip"]))
+  runtime          = var.lambda_runtime
+  description      = var.description
+  timeout          = var.timeout
+  environment {
+    variables = var.lambda_environment_variables
+  }
+  tags = var.resource_tags
+}
---
+++ lambda/variables.tf
@@ -0,0 +1,73 @@
+
+
+
+
+# Required variables:
+
+variable "parent_module_path" {
+  description = "Path of the parent module - to be used for naming resources etc"
+  type        = string
+}
+
+variable "iam_role_arn" {
+  description = "Arn of generated role"
+  type        = string
+}
+
+variable "lambda_script_source_dir" {
+  description = "Directory of lambda package source"
+  type        = string
+}
+
+variable "lambda_script_output_path" {
+  description = "Path to zipped lambda package"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "Handler used by lambda to execute script"
+  type        = string
+}
+
+
+#Optional variables - default values used unless others specified:
+
+variable "child_module" {
+  description = "Name of child module - default value ok if module contains only one lambda"
+  type        = string
+  default     = "main"
+}
+
+variable "lambda_runtime" {
+  description = "Name of child module - default value ok if module contains only one lambda"
+  type        = string
+  default     = "python3.7"
+}
+
+variable "description" {
+  description = "Description of what lambda function does"
+  type        = string
+  default     = "No description given"
+}
+
+variable "timeout" {
+  description = "Timeout limitation for execution of scripts"
+  type        = number
+  default     = 3
+}
+
+variable "lambda_environment_variables" {
+  description = "Defaults to no env variables. If needed, env vars can be given in parent module variables.tf, and assigned in child module call"
+  type        = map(string)
+  default = {
+    "env_var"  = "noen given"
+  }
+}
+
+variable "resource_tags" {
+  description = "Defaults to no tags. If needed, env vars can be given in parent module variables.tf, and assigned in child module call"
+  type        = map(string)
+  default = {
+    "tag"  = "none given"
+  }
+}
