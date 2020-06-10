c173e5a2d092c27aff83091f809e9922312b3a09
Christian-Gudbrandsen <christian.gudbrandsen@ren.oslo.kommune.no>
2020-06-10 22:20:51 +0200

    added step function

---
+++ step_function/main.tf
@@ -0,0 +1,12 @@
+resource "aws_sfn_state_machine" "this" {
+  name     = join("", [basename(var.parent_module_path), "-step_function-", var.added_name])
+  role_arn = var.iam_role_arn
+  definition = templatefile(var.template_file_source_dir,
+    {
+    variableA = var.variableA
+    variableB = var.variableB
+    variableC = var.variableC
+    variableD = var.variableD
+    variableE = var.variableE
+    })
+}
---
+++ step_function/variables.tf
@@ -0,0 +1,49 @@
+# Required variables:
+
+variable "parent_module_path" {
+  description = "Path of the parent module - to be used for naming resources etc"
+  type        = string
+}
+
+variable "iam_role_arn" {
+  description = "Arn of generated role"
+  type        = string
+}
+
+variable "template_file_source_dir" {
+  description = "Path to template file describing state machine"
+  type        = string
+}
+
+
+#Optional variables - default values used unless others specified:
+
+variable "variableA" {
+  description = "Available variable name for passing variable to template file"
+  type        = string
+  default     = "empty"
+}
+
+variable "variableB" {
+  description = "Available variable name for passing variable to template file"
+  type        = string
+  default     = "empty"
+}
+
+variable "variableC" {
+  description = "Available variable name for passing variable to template file"
+  type        = string
+  default     = "empty"
+}
+
+variable "variableD" {
+  description = "Available variable name for passing variable to template file"
+  type        = string
+  default     = "empty"
+}
+
+variable "variableE" {
+  description = "Available variable name for passing variable to template file"
+  type        = string
+  default     = "empty"
+}
