d18bd9eb53eb5d027901d080c5dd5fc28518c012
Christian-Gudbrandsen <christian.gudbrandsen@ren.oslo.kommune.no>
2020-06-26 00:13:59 +0200

    updated naming convention for iam role

--- iam/role/main.tf
+++ iam/role/main.tf
@@ -3,7 +3,7 @@
 
 # Generates the IAM role with policy to be used by the resource
 resource "aws_iam_role" "this" {
-  name                 = join("", [basename(var.parent_module_path), "-iam_role-", var.added_name])
+  name                 = join("", [basename(var.parent_module_path), "-", basename(path.module)])
   assume_role_policy   = var.assume_policy_path
   description          = var.description
   permissions_boundary = var.permission_boundary
@@ -12,7 +12,7 @@ resource "aws_iam_role" "this" {
 
 # Generates the role policy to be used by the resource
 resource "aws_iam_role_policy" "this" {
-  name   = join("", [basename(var.parent_module_path), "-iam_policy-", var.added_name])
+  name   = join("", [basename(var.parent_module_path), "-", basename(path.module),"-policy"])
   role   = aws_iam_role.this.id
   policy = var.policy_path
 }
