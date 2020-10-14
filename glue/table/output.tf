037f82440f7780f441caef3cea67d2eb91123c0e
Aleksandr Sedyshev <aleksandr.siedyshev@ren.oslo.kommune.no>
2020-10-14 11:43:14 +0200

    bug source_type

--- glue/table/output.tf
+++ glue/table/output.tf
@@ -1,11 +1,11 @@
 output "arn" {
-  value = (var.table_type == "json") ? aws_glue_catalog_table.json_table[0].arn : aws_glue_catalog_table.csv_table[0].arn 
+  value = (var.source_type == "json") ? aws_glue_catalog_table.json_table[0].arn : aws_glue_catalog_table.csv_table[0].arn 
 }
 
 output "id" {
-  value = (var.table_type == "json") ? aws_glue_catalog_table.json_table[0].id : aws_glue_catalog_table.csv_table[0].id 
+  value = (var.source_type == "json") ? aws_glue_catalog_table.json_table[0].id : aws_glue_catalog_table.csv_table[0].id 
 }
 
 output "name" {
-  value = (var.table_type == "json") ? aws_glue_catalog_table.json_table[0].name : aws_glue_catalog_table.csv_table[0].name 
+  value = (var.source_type == "json") ? aws_glue_catalog_table.json_table[0].name : aws_glue_catalog_table.csv_table[0].name 
 }
