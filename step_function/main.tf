005f0f4b677812efceacfd130fb12e4026bca2c7
Aleksandr Sedyshev <aleksandr.siedyshev@ren.oslo.kommune.no>
2020-10-15 14:26:58 +0200

    step function. remove parameters

--- step_function/main.tf
+++ step_function/main.tf
@@ -1,18 +1,6 @@
 resource "aws_sfn_state_machine" "this" {
-  name     = join("", [basename(var.parent_module_path), "-", var.module_name])
-  role_arn = var.iam_role_arn
-  definition = templatefile(var.template_file_source_dir,
-    {
-    variableA = var.variableA
-    variableB = var.variableB
-    variableC = var.variableC
-    variableD = var.variableD
-    variableE = var.variableE
-    variableF = var.variableF
-    variableG = var.variableG
-    variableH = var.variableH
-    variableI = var.variableI
-    variableJ = var.variableJ
-    })
-  tags = var.resource_tags
+  name       = join("", [basename(var.parent_module_path), "-", var.module_name])
+  role_arn   = var.iam_role_arn
+  definition = var.template_file
+  tags       = var.resource_tags
 }
--- step_function/variables.tf
+++ step_function/variables.tf
@@ -10,7 +10,7 @@ variable "iam_role_arn" {
   type        = string
 }
 
-variable "template_file_source_dir" {
+variable "template_file" {
   description = "Path to template file describing state machine"
   type        = string
 }
@@ -21,67 +21,6 @@ variable "module_name" {
 }
 
 #Optional variables - default values used unless others specified:
-
-variable "variableA" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableB" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableC" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableD" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableE" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableF" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableG" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableH" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableI" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
-variable "variableJ" {
-  description = "Available variable name for passing variable to template file"
-  type        = string
-  default     = "empty"
-}
-
 variable "resource_tags" {
   description = "Defaults to no tags. If needed, env vars can be given in parent module variables.tf, and assigned in child module call"
   type        = map(string)
