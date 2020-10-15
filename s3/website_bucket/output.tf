53b6515332e54ea5ca3e48dab428b49faec732f6
Aleksandr Sedyshev <aleksandr.siedyshev@ren.oslo.kommune.no>
2020-10-15 19:51:18 +0200

    output. hosted_zone_id

--- s3/website_bucket/output.tf
+++ s3/website_bucket/output.tf
@@ -17,3 +17,13 @@ output "website_endpoint" {
   description = "Bucket bucket"
   value       = aws_s3_bucket.this.website_endpoint
 }
+
+output "website_domain" {
+  description = "Bucket bucket"
+  value       = aws_s3_bucket.this.website_domain
+}
+
+output "hosted_zone_id" {
+  description = "Bucket bucket"
+  value       = aws_s3_bucket.this.hosted_zone_id
+}
