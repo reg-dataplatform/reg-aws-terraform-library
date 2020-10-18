4029166f67094e7a30a9d9161126ae4c5bf12caa
Aleksandr Sedyshev <aleksandr.siedyshev@ren.oslo.kommune.no>
2020-10-18 19:58:59 +0200

    add bucket_regional_domain_name

--- s3/website_bucket/output.tf
+++ s3/website_bucket/output.tf
@@ -1,29 +1,27 @@
 output "arn" {
-  description = "Bucket arn"
-  value       = aws_s3_bucket.this.arn
+  value = aws_s3_bucket.this.arn
 }
 
 output "name" {
-  description = "Bucket name"
-  value       = aws_s3_bucket.this.bucket
+  value = aws_s3_bucket.this.bucket
 }
 
 output "id" {
-  description = "Bucket id"
-  value       = aws_s3_bucket.this.id
+  value = aws_s3_bucket.this.id
 }
 
 output "website_endpoint" {
-  description = "Bucket bucket"
-  value       = aws_s3_bucket.this.website_endpoint
+  value = aws_s3_bucket.this.website_endpoint
 }
 
 output "website_domain" {
-  description = "Bucket bucket"
-  value       = aws_s3_bucket.this.website_domain
+  value = aws_s3_bucket.this.website_domain
 }
 
 output "hosted_zone_id" {
-  description = "Bucket bucket"
-  value       = aws_s3_bucket.this.hosted_zone_id
+  value = aws_s3_bucket.this.hosted_zone_id
+}
+
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.this.bucket_regional_domain_name
 }
