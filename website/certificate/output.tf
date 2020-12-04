0f36aa4aeffc3b1a9e28ebb20bdf792f2aaaedb7
Aleksandr Sedyshev <aleksandr.siedyshev@reg.oslo.kommune.no>
2020-12-04 14:59:07 +0100

    fix bug output[0] when destroy service

--- website/certificate/output.tf
+++ website/certificate/output.tf
@@ -1,6 +1,6 @@
 output "certificate_arn"         {value = aws_acm_certificate.this.arn}
 output "certificate_id"          {value = aws_acm_certificate.this.id}
 output "certificate_domain_name" {value = aws_acm_certificate.this.domain_name}
-output "route53_record_zone_id"  {value = values(aws_route53_record.this)[0].zone_id}
-output "route53_record_name"     {value = values(aws_route53_record.this)[0].name}
-output "route53_record_type"     {value = values(aws_route53_record.this)[0].type}
+output "route53_record_zone_id"  {value = values(aws_route53_record.this)[*].zone_id}
+output "route53_record_name"     {value = values(aws_route53_record.this)[*].name}
+output "route53_record_type"     {value = values(aws_route53_record.this)[*].type}
