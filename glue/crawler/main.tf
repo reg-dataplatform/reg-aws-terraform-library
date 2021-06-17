4a0b773a7078118ac2e847f97654d202cf26b0fa
Jon Omnes <omnjon@gmail.com>
2021-06-17 11:39:12 +0200

    conditional crawler settings

--- glue/crawler/main.tf
+++ glue/crawler/main.tf
@@ -3,21 +3,21 @@ locals {
 }
 
 resource "aws_glue_crawler" "this" {
-  database_name  = var.database_name
-  name           = local.crawler_name
-  role           = var.iam_role_arn
-  tags           = var.resource_tags
-  schedule       = var.cron_schedule
-  table_prefix   = var.prefix
+  database_name = var.database_name
+  name          = local.crawler_name
+  role          = var.iam_role_arn
+  tags          = var.resource_tags
+  schedule      = var.cron_schedule
+  table_prefix  = var.prefix
   schema_change_policy {
-    delete_behavior = var.delete_behavior
-    update_behavior = var.update_behavior
+    delete_behavior = (var.delete_behavior != "" ? var.delete_behavior : null)
+    update_behavior = (var.update_behavior != "" ? var.update_behavior : null)
   }
   recrawl_policy {
-    recrawl_behavior = var.recrawl_behavior
+    recrawl_behavior = (var.recrawl_behavior != "" ? var.recrawl_behavior : null)
   }
   s3_target {
     path = var.s3_source_path
   }
-  # configuration = configuration
+  configuration = (var.config != "" ? var.config : null)
 }
